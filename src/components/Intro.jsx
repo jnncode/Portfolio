7bbd55e262dfb761c3826358df97eb47284dd036
J Nguyen <n.nguyenj@yahoo.com>
2023-06-16 01:37:16 -0500

    Revisions and Updates
    - Removed About (kept it simple)
    - Revised Intro to include a fade in-out animation
    - Polished some other code snippets

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 function Intro() {
+    const [isAnimating, setAnimating] = useState(true);
+
+    const animation = useSpring ({
+        from: { opacity: isAnimating ? 0 : 1 }, // initial state
+        to: { opacity: isAnimating ? 1 : 0 }, // final state
+        config: { duration: 1000 }, // animation duration in ms
+        onRest: () => {
+            // restarts the animation once completed
+            setAnimating(!isAnimating);
+        },
+    });
+
     return (
-        <div>
-            <h1></h1>
-        </div>
+        <animated.div style={animation}>
+            <div className='flex justify-center items-center h-screen'>
+                <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer &amp;Content Creator</p>
+            </div>
+        </animated.div>
+        
     )
 }
 
--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function TimelineItem({ year, title, duration, description }) {
+function TimelineItem({ year, title, description }) {
     return (
         <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
             <li className='mb-10 ml-4'>
