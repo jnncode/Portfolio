70a5b893bcbe67eaa5870452f8849e07e879d046
jnncode <n.nguyenj.media@proton.me>
2023-12-23 19:44:17 -0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.env.NODE_END === 'production' ? '/Portfolio' : '',
