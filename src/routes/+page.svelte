eb52f541e9ed17edfda9f9d407bcfe0f29665044
jnncode <n.nguyenj.media@proton.me>
2024-01-10 23:45:10 -0600

    Design Revisions
    - Revised Home Page to be more simplistic
    - Revised Work Page to focus on the important projects with features of prints to download
    - Added link to VSCO gallery in About rather than including the pieces into Work Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+    const events = [
+        { period: 'September 2023 - December 2023', title: 'Graphic Design Intern', description: 'Distinguished in refining visual identities to enhance audience engagement and maintain consistency across diverse subjects. Crafted visual elements following design principles to contribute to the desgin of an e-book. Integrated strategic business objectives alongside the author to ensure UX.'},
+        { period: 'August 2023 - September 2023', title: 'Software Engineer Intern', description: 'Led design initiatives with business objectives and user needs to enhance user satisfaction. Conducted user research, usability testing, and data analysis, to optimize usability. Collaborated on integration of user-centric design principles throughout the development cycle.'},
+        { period: 'March 2022 - April 2023', title: 'Software Engineer Intern', description: 'Developed and tested regression and automation suites for Thomson Reuters (TR), optimizing QA processes. Collaborated on optimizing architecture of TR’s web-based, ensuring scalability. Delivered production components, facilitating smooth deployment of key features.'},
+        { period: 'August 2019 - December 2023', title: 'Bachelor of Science in Computer Information Technology', description: 'Attained a degree with a primary focus on Information Technology and Computer Science, complemented by a minor in Business Administration at Minnesota State University, Mankato.'},
+    ]
+</script>
+
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="JNNCODE" />
@@ -6,35 +15,11 @@
 <div class="container">
     <ul class="timeline-list">
         <li class="timeline-item">
-            <h4 class="timeline-period">September 2023 - December 2023</h4>
-            <h3 class="timeline-title">Graphic Design Intern</h3>
-            <p class="timeline-description">
-                Led design initiatives with business objectives and user needs to enhance user satisfaction. Conducted user research, usability testing, and data analysis, to optimize usability. 
-                Collaborated on integration of user-centric design principles throughout the development cycle.
-            </p>
-        </li>
-        <li class="timeline-item">
-            <h4 class="timeline-period">August 2023 - September 2023</h4>
-            <h3 class="timeline-title">Software Engineer Intern</h3>
-            <p class="timeline-description">
-                Led design initiatives with business objectives and user needs to enhance user satisfaction. Conducted user research, usability testing, and data analysis, to optimize usability. 
-                Collaborated on integration of user-centric design principles throughout the development cycle.
-            </p>
-        </li>
-        <li class="timeline-item">
-            <h4 class="timeline-period">March 2022 - April 2023</h4>
-            <h3 class="timeline-title">Software Engineer Intern</h3>
-            <p class="timeline-description">
-                Developed and tested regression and automation suites for Thomson Reuters (TR), optimizing QA processes. Collaborated on optimizing architecture of TR’s web-based, ensuring scalability. 
-                Delivered production components, facilitating smooth deployment of key features.
-            </p>
-        </li>
-        <li class="timeline-item">
-            <h4 class="timeline-period">August 2019 - December 2023</h4>
-            <h3 class="timeline-title">Bachelor of Science in Computer Information Technology</h3>
-            <p class="timeline-description">
-                Attained a degree with a primary focus on Information Technology and Computer Science, complemented by a minor in Business Administration at Minnesota State University, Mankato.
-            </p>
+            {#each events as {period, title, description}}
+                <h4 class="timeline-period">{period}</h4>
+                <h3 class="timeline-title">{title}</h3>
+                <p class="timeline-description">{description}</p>
+            {/each}
         </li>
     </ul>
 </div>
@@ -50,10 +35,7 @@
         position: relative;
     }
     .timeline-period {
-        border-color: #ffffff;
-        border-style: solid;
         color: #ffffff;
-        width: 18rem;
-        text-align: center;
+        text-decoration: underline;
     }
 </style>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,11 +1,25 @@
 <script>
     import { assetsBasePath } from "$lib/path.js";
 
-    const photos = [
-        { photoLink: `https://vsco.co/jnncode/media/65612a2cb3e2174e1ada2871`, photoSource: `${assetsBasePath}images/GameBoy.jpeg` },
-        { photoLink: `https://vsco.co/jnncode/media/658d003ede26f90b3ed77142`, photoSource: `${assetsBasePath}images/ChinaTown.jpeg` },
-        { photoLink: `https://vsco.co/jnncode/media/658d0032de26f90b3ed77141`, photoSource: `${assetsBasePath}images/Tokyo.jpeg` },
-        { photoLink: `https://vsco.co/jnncode/media/658cfff8de26f90b3ed77140`, photoSource: `${assetsBasePath}images/Astronaut.jpeg` },
+    const slides = [
+        { id: 1, image: `${assetsBasePath}images/Spotify.png`, title: 'Mocked Spotify Web Player', caption: 'Scraped API and integrated OAuth regeneration.'},
+        { id: 2, image: `${assetsBasePath}images/FittsLaw.png`, title: `Fitts' Law`, caption: 'Simulated time construction following heuristic evaluation practices.'},
+        { id: 3, image: `${assetsBasePath}images/RunAstro.png`, title: 'Run Astro', caption: 'Designed adventure of low-poly astronaut world unlocking hidden super powers.'},
+    ]
+    let index = 0;
+
+    function next() {
+        index = (index + 1) % slides.length;
+    }
+    function previous() {
+        index = (index - 1 + slides.length) % slides.length;
+    }
+
+    const prints = [
+        { link: `https://mega.nz/folder/dfNDRabR#_UzhseJShbbeTeXfSQsBFQ`, source: `${assetsBasePath}icons/Folder.svg`, caption: 'Studio Ghibli', extra: 'Tree' },
+        { link: `https://mega.nz/folder/4C9DUTIY#czQ4_uWA4iT5sAQSdMKpRw`, source: `${assetsBasePath}icons/Folder.svg`, caption: 'Bonsai Tree', extra: '' },
+        { link: `https://mega.nz/folder/cHVgHCRY#hYfzz2UxobfqUAz_CofFPA`, source: `${assetsBasePath}icons/Folder.svg`, caption: 'Cherry Blossom', extra: 'Tree' },
+        { link: `https://mega.nz/folder/hfUgzSJT#_SIXHoXnaT2asS2vtHSw3g`, source: `${assetsBasePath}icons/Folder.svg`, caption: 'Lotus Flower', extra: '' },
     ];
 </script>
 
@@ -15,35 +29,31 @@
 </svelte:head>
 
 <div class="container">
-    <h4 class="section">
-        Codes
-    </h4>
+    <h4>Codes</h4>
     <div class="slideshow">
-        <div class="slides">
-            <img src="{assetsBasePath}images/Spotify.png" class="slide-img" alt="slide 1" />
-            <div class="slide-text">
-                <div class="title">Mocked Spotify Web Player</div>
-                <div class="description">Scrape API</div>
-            </div>
-        </div>
-        <div class="arrows">
-            <!-- <a class="previous" on:click="{next}">&#10094;</a>
-            <a class="next" on:click="{previous}">&#10095;</a> -->
-        </div>
+        <img class="slide" src={slides[index].image} alt={[index]} />
+        <div class="title">{slides[index].title}</div>
+        <div class="caption">{slides[index].caption}</div>
+        <button id="prev" on:click={previous}>&#10094;</button>
+        <button id="next" on:click={next}>&#10095;</button>
     </div>
-    <h4 class="section">
-        Photos
-    </h4>
+    <h4>Prints</h4>
     <div class="grid">
-        {#each photos as {photoLink, photoSource}}
-            <a href={photoLink} target="_blank" rel="noopener noreferrer">
-                <img src={photoSource} class="photos" alt="photos" />
+        {#each prints as {link, caption, extra}}
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="125px" height="125px">
+                    <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                        <g transform="scale(8.53333,8.53333)">
+                            <path d="M4,3c-1.105,0 -2,0.895 -2,2v3h11h15v-1c0,-1.105 -0.895,-2 -2,-2h-14.80078l-0.61719,-1.0293c-0.361,-0.602 -1.01184,-0.9707 -1.71484,-0.9707zM3,10c-0.552,0 -1,0.448 -1,1v12c0,1.105 0.895,2 2,2h22c1.105,0 2,-0.895 2,-2v-12c0,-0.552 -0.448,-1 -1,-1z"></path>
+                        </g>
+                    </g>
+                    <text class="text" x="25" y="125" fill="#000000">{caption}
+                        <tspan class="text" x="25" y="150">{extra}</tspan>
+                    </text>
+                </svg>
             </a>
         {/each}
     </div>
-    <h4 class="section">
-        Prints
-    </h4>
 </div>
 
 <style>
@@ -53,54 +63,76 @@
         justify-content: center;
         align-items: center;
     }
-    .section {
-        border-color: #ffffff;
-        border-style: solid;
-        color: #ffffff;
-        width: 18rem;
-        text-align: center;
-    }
     .slideshow {
         margin: 0;
-    }
-    .slide-img {
-        max-width: 100%;
-        height: auto;
-    }
-    .slide-text {
         position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         text-align: center;
     }
+    img {
+        max-width: 100%;
+        max-height: 100%;
+        width: 45rem;
+        height: 25rem;
+        object-fit: cover;
+    }
+    .title, .caption {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: #9e9e9e;
+    }
     .title {
+        bottom: 20%;
         font-weight: 900;
+        white-space: nowrap;
     }
-    .description {
-        font-weight: 600;
-        font-size: small;
+    .caption {
+        bottom: 10%;
+        font-weight: 400;
     }
+    button {
+        position: absolute;
+        font-size: 2rem;
+        width: 3rem;
+        padding: 0.25rem;
+        font-weight: 900;
+        color: #ffffff;
+        background-color: transparent;
+        border: none;
+    }
+    button:hover {
+        cursor: pointer;
+        font-weight: 900;
+        color: #bdbdbd;
+    }
+    #prev {
+        left: 0;
+    }
+
+    #next {
+        right: 0;
+    }
+
     .grid {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
-        gap: 1rem;
-        color: #ffffff;
+        gap: 5rem;
     }
-    .photos {
-        border-color: #ffffff;
-        border-style: solid;
-        border-width: 0.15rem;
-        width: 12rem;
-        height: 12rem;
+    .grid a {
+        /* Same as icon dimensions */
+        height: 125px;
+        width: 125px;
+        text-decoration: none;
     }
-    .arrows {
-        position: relative;
-        text-align: center;
-        top: 0.75rem;
+    .grid a:focus {
+        outline: none;
     }
-    /* .previous, .next {
-        cursor: pointer;
-        font-weight: 900;
-        color: #ffffff;
-    } */
+    .text {
+        font-size: 26px;
+    }
     @media (max-width: 768px) {
         .grid {
             grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
